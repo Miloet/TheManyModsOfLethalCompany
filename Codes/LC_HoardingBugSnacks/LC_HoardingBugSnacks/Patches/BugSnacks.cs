ed322ba175c5e3475003b249e4b83546e1b0d7ea
MiloZF <Miloet00@gmail.com>
2024-02-12 16:10:29 +0100

    a

--- Codes/LC_HoardingBugSnacks/LC_HoardingBugSnacks/Patches/BugSnacks.cs
+++ Codes/LC_HoardingBugSnacks/LC_HoardingBugSnacks/Patches/BugSnacks.cs
@@ -1,12 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using GameNetcodeStuff;
+using UnityEngine;
+using HoarderBud.Patches;
+using HoarderBud;
+using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
 
 namespace LC_HoardingBugSnacks.Patches
 {
-    internal class BugSnacks
+    public class BugSnacks : GrabbableObject
     {
+
+        public override void GrabItemFromEnemy(EnemyAI enemy)
+        {
+            if (enemy is HoarderBugAI)
+            {
+                var bug = (HoarderBugAI)enemy;
+                BecomeFriends(bug, 15f);
+            }
+        }
+
+        public static void BecomeFriends(HoarderBugAI mainBug, float friendAmount, bool allBugs = true)
+        {
+            mainBug.BecomeFriends(friendAmount);
+
+            if (allBugs)
+                foreach (HoarderBugAI bug in FindObjectsOfType<HoarderBugAI>())
+                {
+                    bug.BecomeFriends(friendAmount);
+                }
+        }
+    }
+
+    public static class ExtensionMethod
+    {
+        public static void BecomeFriends(this HoarderBugAI original, float amount)
+        {
+            Type type = typeof(HoarderBugAI);
+
+            // Get the private field
+            FieldInfo field = type.GetField("annoyanceMeter", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (field != null)
+            {
+                // Get the value of the private field
+                float currentValue = (float)field.GetValue(original);
+
+                // Modify the value of the private field
+                field.SetValue(original, currentValue - amount);
+            }
+            original.angryTimer -= amount;
+        }
+
+        public static IEnumerator<WaitForEndOfFrame> Dance(this HoarderBugAI original, float time)
+        {
+            float Amplitude = 2;
+            float Frequency = 5;
+
+            Transform spinTrans = original.gameObject.transform.Find("HoarderBugModel").Find("AnimContainer").Find("Armature");
+            Transform chestTrans = spinTrans.Find("Abdomen").Find("Chest");
+            Vector3 startRotation = spinTrans.localRotation.eulerAngles;
+
+            float startY = chestTrans.position.y;
+            while (time < 0)
+            {
+                chestTrans.localPosition = new Vector3(
+                    chestTrans.localPosition.x, 
+                    startY + Mathf.Sin(time * Frequency) * Amplitude,
+                    chestTrans.localPosition.z);
+
+                spinTrans.localRotation = Quaternion.Euler(startRotation.x, startRotation.y + time, startRotation.z);
+
+                time += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+        }
     }
 }
---
+++ Codes/LC_HoardingBugSnacks/LC_HoardingBugSnacks/Patches/HoarderBugAIPatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC_HoardingBugSnacks.Patches
+{
+    [HarmonyPatch(typeof(RoundManager))]
+    internal class HoarderBugAIPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public void SpawnWithShotgun()
+        {
+            float chance = 0.01f;
+            if(chance < Random.Range(0f,1f))
+            {
+                //Give the bug a shotgun
+            }
+        }
+
+    }
+}
