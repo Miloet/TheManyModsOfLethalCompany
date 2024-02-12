f8c21448c1cd582a0ff182f8b3a05a46b840c217
MiloZF <Miloet00@gmail.com>
2024-02-12 13:21:52 +0100

    bug snacks initial

--- Codes/LC_HoardingBugSnacks/LC_HoardingBugSnacks/Properties/AssemblyInfo.cs
+++ Codes/LC_HoardingBugSnacks/LC_HoardingBugSnacks/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LC_BugSnacks")]
+[assembly: AssemblyTitle("LC_HoardingBugSnacks")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LC_BugSnacks")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCompany("Aca")]
+[assembly: AssemblyProduct("LC_HoardingBugSnacks")]
+[assembly: AssemblyCopyright("Copyright © Aca 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d6700c5d-017d-454f-830b-9d752ea84fd6")]
+[assembly: Guid("8a19c370-df01-4355-878e-94a5f517f97b")]
 
 // Version information for an assembly consists of the following four values:
 //
