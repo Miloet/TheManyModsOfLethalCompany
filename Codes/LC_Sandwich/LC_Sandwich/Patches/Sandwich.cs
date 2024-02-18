f60cd3149f0c55ec379e40a6a948cf4aa2d332f3
Miloet <miloet00@gmail.com>
2024-02-18 22:57:32 +0100

    sandwich complete*

---
+++ Codes/LC_Sandwich/LC_Sandwich/Patches/Sandwich.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using GameNetcodeStuff;
+
+namespace LC_Sandwich
+{
+    public class Sandwich : PhysicsProp
+    {
+        //Settings
+
+        public static int sandwichSize = 4; //Amount of times the sandwich can be eaten
+        public static float timeToEat = 2f;
+        public static int healing = 15;
+
+        public float eatingSpeed = 1f;
+        private float eaten = 0; // 0 - 1 value describing how much of it has been eaten
+        private int originalValue;
+
+        Material material;
+        AudioSource audioSource;
+        public static AudioClip eatingSFX;
+        public static AudioClip finishSFX;
+
+        public bool eating;
+
+        private PlayerControllerB previousPlayerHeldBy;
+
+        private Coroutine startEating;
+
+        public static Vector3 defaultRotation;
+        public static Vector3 defaultOffset;
+
+        public static Item originalProperties;
+        public static Item eatingProperties;
+
+        public override void Start()
+        {
+            base.Start();
+            grabbable = true;
+            
+            useCooldown = 0.3f;
+            originalValue = scrapValue;
+            material = GetComponent<MeshRenderer>().material;
+            material.SetInt("_Size", sandwichSize);
+
+            UpdateEating(0);
+
+            audioSource = GetComponent<AudioSource>();
+
+            eatingSpeed = sandwichSize * timeToEat;
+
+            itemProperties = originalProperties;
+            insertedBattery = new Battery(false, 0f);
+        }
+
+        public override void ItemActivate(bool used, bool buttonDown = true)
+        {
+            base.ItemActivate(used, buttonDown);
+
+            if (buttonDown)
+            {
+                isBeingUsed = true;
+                startEating = StartCoroutine(StartEating());
+                previousPlayerHeldBy.activatingItem = true;
+                previousPlayerHeldBy.playerBodyAnimator.SetBool("useTZPItem", true);
+            }
+            else
+            {
+                isBeingUsed = false;
+                
+                if (startEating != null)
+                {
+                    StopCoroutine(startEating);
+                }
+                Stop();
+            }
+        }
+        public override void Update()
+        {
+            base.Update();
+
+            if(eating)
+            {
+                if (previousPlayerHeldBy == null || !isHeld || eaten > 1f)
+                {
+                    eating = false;
+                }
+                UpdateEating(Mathf.MoveTowards(eaten, 1, Time.deltaTime / eatingSpeed));
+                
+            }
+            if (isHeld && previousPlayerHeldBy == StartOfRound.Instance.localPlayerController)
+            {
+                insertedBattery.charge = eaten;
+                if (base.IsOwner) SyncBatteryServerRpc((int)(insertedBattery.charge * 100f));
+            }
+            else
+            {
+                eaten = insertedBattery.charge;
+            }
+             
+            if (eaten >= 1)
+            {
+                audioSource.PlayOneShot(finishSFX);
+                RoundManager.Instance.PlayAudibleNoise(base.transform.position, 15f, 1.5f, 0, isInElevator && StartOfRound.Instance.hangarDoorsClosed);
+                if (base.IsOwner) SyncBatteryServerRpc(100);
+                DestroyObjectInHand(previousPlayerHeldBy);
+            }
+        }
+
+        public void UpdateEating(float newValue)
+        {
+            if (Mathf.Floor(newValue * sandwichSize) / sandwichSize != Mathf.Floor(eaten * sandwichSize) / sandwichSize)
+            {
+                //play funny sound
+                Stop();
+                previousPlayerHeldBy.health = Mathf.Clamp(previousPlayerHeldBy.health + healing, 0, 100);
+                HUDManager.Instance.UpdateHealthUI(previousPlayerHeldBy.health, false);
+            }
+            
+            eaten = newValue;
+            material.SetFloat("_Eating", eaten);
+            scrapValue = (int)Mathf.Lerp(0, originalValue, eaten);
+        }
+
+        public override void EquipItem()
+        {
+            base.EquipItem();
+            if (playerHeldBy != null)
+            {
+                previousPlayerHeldBy = playerHeldBy;
+            }
+        }
+        public override void DiscardItem()
+        {
+            if (startEating != null)
+            {
+                StopCoroutine(startEating);
+            }
+            Stop();
+
+            if (previousPlayerHeldBy != null)
+            {
+                previousPlayerHeldBy.activatingItem = false;
+            }
+            base.DiscardItem();
+        }
+
+        public void Stop()
+        {
+            eating = false;
+            previousPlayerHeldBy.activatingItem = false;
+            audioSource.Stop();
+            previousPlayerHeldBy.playerBodyAnimator.SetBool("useTZPItem", false);
+            itemProperties = originalProperties;
+            eatingProperties.rotationOffset = defaultRotation;
+            eatingProperties.positionOffset = defaultOffset;
+        }
+
+
+        public static Vector3 fullSandwichEatingPosition = 
+            new Vector3(-0.2f, 0.3f, 0.2f);
+        public static Vector3 OneBiteLeftSandwichEatingPosition = 
+            new Vector3(0.5f, 0.2f, -0.4f);
+        public static Vector3 SandwichEatingRotation = 
+            new Vector3(0, -45, 0);
+
+
+        public IEnumerator<WaitForEndOfFrame> StartEating()
+        {
+            float time = 0;
+
+            float duration = .5f;
+
+            Vector3 position = Vector3.Lerp(fullSandwichEatingPosition, OneBiteLeftSandwichEatingPosition, Mathf.Floor(eaten * sandwichSize) / sandwichSize);
+            itemProperties = eatingProperties;
+            while (time < duration)
+            {
+                float t = Mathf.InverseLerp(0, duration, time);
+
+                eatingProperties.rotationOffset = Vector3.Lerp(defaultRotation, SandwichEatingRotation, t);
+                eatingProperties.positionOffset = Vector3.Lerp(defaultOffset, position, t);
+
+                time += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+            eating = true;
+            audioSource.PlayOneShot(eatingSFX);
+            audioSource.pitch = eatingSFX.length / timeToEat;
+            RoundManager.Instance.PlayAudibleNoise(base.transform.position, 10f, 1f, 0, isInElevator && StartOfRound.Instance.hangarDoorsClosed);
+        }
+
+
+    }
+}
---
+++ Codes/LC_Sandwich/LC_Sandwich/Plugin.cs
@@ -0,0 +1,109 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalLib.Modules;
+using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace LC_Sandwich
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class SandwichMod : BaseUnityPlugin
+    {
+
+        private const string modGUID = "Mellowdy.YummySandwich";
+        private const string modName = "YummySandwich";
+        private const string modVersion = "0.0.1";
+
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static ManualLogSource mls;
+
+        private static SandwichMod instance;
+        public static AssetBundle assets;
+
+        public static string assetName = "sandwich.asset";
+        public static string itemName = "Sandwich.asset";
+        public static string prefabName = "Sandwich.prefab";
+
+        public static int rarity = 30;
+
+        void Awake()
+        {
+            if (instance == null) instance = this;
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            //Load Asset Bundle
+            string currentDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string path = Path.Combine(currentDirectory, assetName).Replace("\\", "/");
+            assets = AssetBundle.LoadFromFile(path);
+
+            //Item
+
+            GameObject itemPrefab = assets.LoadAsset<GameObject>(prefabName);
+
+            Sandwich comp = itemPrefab.AddComponent<Sandwich>();
+            Item sandwichItem = assets.LoadAsset<Item>(itemName);
+
+            Sandwich.eatingSFX = assets.LoadAsset<AudioClip>("Eat.wav");
+            Sandwich.finishSFX = assets.LoadAsset<AudioClip>("FinishEating.wav");
+
+            Sandwich.defaultOffset = sandwichItem.positionOffset;
+            Sandwich.defaultRotation = sandwichItem.rotationOffset;
+
+            Sandwich.originalProperties = sandwichItem;
+            Sandwich.eatingProperties = Instantiate(sandwichItem);
+
+            Sandwich.sandwichSize = Config.Bind<int>("Properties", "Size", 4, "How many times can the sandwich be eaten").Value;
+            Sandwich.healing = Config.Bind<int>("Properties", "Healing", 35, "How many hitpoints should be restored when one part is consumed").Value;
+            Sandwich.timeToEat = Config.Bind<int>("Properties", "Time", 2, "How long in seconds does it take to eat one part of the sandwich").Value;
+
+
+
+
+            comp.itemProperties = sandwichItem;
+            sandwichItem.spawnPrefab = itemPrefab;
+
+
+            rarity = Config.Bind<int>("Rarity", "Base rarity", 30, "Rarity of the sandwich between 1 - 100 (for context, on march: bottles/large axel/engine: 80 - 100. gold bar/robot/lazer pointer: 1 - 6)").Value;
+
+            NetworkPrefabs.RegisterNetworkPrefab(sandwichItem.spawnPrefab);
+
+            Items.RegisterScrap(sandwichItem, (int)(rarity * 
+                Config.Bind<float>("Rarity", "Experimentation", 0.3f, "Rarity multiplier for Experimentation").Value),
+                 Levels.LevelTypes.ExperimentationLevel);
+            Items.RegisterScrap(sandwichItem, (int)(rarity * 
+                Config.Bind<float>("Rarity", "Assurance", 0f, "Rarity multiplier for Assurance").Value),
+                Levels.LevelTypes.AssuranceLevel);
+            Items.RegisterScrap(sandwichItem, (int)(rarity * 
+                Config.Bind<float>("Rarity", "Vow", 1.5f, "Rarity multiplier for Vow").Value),
+                Levels.LevelTypes.VowLevel);
+
+            Items.RegisterScrap(sandwichItem, (int)(rarity * 
+                Config.Bind<float>("Rarity", "Offense", 1f, "Rarity multiplier for Offense").Value),
+                    Levels.LevelTypes.OffenseLevel);
+            Items.RegisterScrap(sandwichItem, (int)(rarity * 
+                Config.Bind<float>("Rarity", "March", 2f, "Rarity multiplier for March").Value),
+                            Levels.LevelTypes.MarchLevel);
+
+            Items.RegisterScrap(sandwichItem, (int)(rarity * 
+                Config.Bind<float>("Rarity", "Rend", 1f, "Rarity multiplier for Rend").Value),
+                            Levels.LevelTypes.RendLevel);
+            Items.RegisterScrap(sandwichItem, (int)(rarity * 
+                Config.Bind<float>("Rarity", "Dine", 1f, "Rarity multiplier for Dine").Value),
+                            Levels.LevelTypes.DineLevel);
+            Items.RegisterScrap(sandwichItem, (int)(rarity * 
+                Config.Bind<float>("Rarity", "Titan", .5f, "Rarity multiplier for Titan").Value),
+                            Levels.LevelTypes.TitanLevel);
+
+
+
+            harmony.PatchAll();
+            mls.LogInfo($"{modName} has been loaded");
+        }
+    }
+}
