c8c2b0956a754111d9ee62da1db9024cced05d06
MiloZF <Miloet00@gmail.com>
2024-03-04 16:43:15 +0100

    a

--- Codes/LC_HoardingBugSnacks/LC_HoardingBugSnacks/Patches/BugSnacks.cs
+++ Codes/LC_HoardingBugSnacks/LC_HoardingBugSnacks/Patches/BugSnacks.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using GameNetcodeStuff;
@@ -9,21 +10,13 @@ using UnityEngine;
 
 namespace LC_HoardingBugSnacks.Patches
 {
-    public class BugSnacks : PhysicsProp
+    public class BugSnacks : GrabbableObject
     {
         public static float BugHappiness;
 
         public override void Start()
         {
             base.Start();
-            foreach (var g in FindObjectsOfType<GrabbableObject>())
-            {
-                HoardingBugSnacksMod.mls.LogMessage($"{g.itemProperties.itemName}" +
-                    $"\n\tgrabAnim: {g.itemProperties.grabAnim}" +
-                    $"\n\tpocketAnim: {g.itemProperties.pocketAnim}" +
-                    $"\n\tuseAnim: {g.itemProperties.useAnim}" +
-                    $"\n\tthrowAnim: {g.itemProperties.throwAnim}");
-            }
         }
 
         public override void ItemActivate(bool used, bool buttonDown = true)
@@ -32,8 +25,14 @@ namespace LC_HoardingBugSnacks.Patches
             if (buttonDown)
             {
                 playerHeldBy.playerBodyAnimator.SetTrigger("shakeItem");
-                MakeBugsDance(transform, 2, 10);
+                MakeBugsDance(transform, 1, 5);
             }
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            HoardingBugSnacksMod.mls.LogMessage("HOW HAPPY ARE THE BUGS: "+ BugHappiness);
         }
 
         public override void GrabItemFromEnemy(EnemyAI enemy)
@@ -42,19 +41,19 @@ namespace LC_HoardingBugSnacks.Patches
             if (enemy != null && enemy is HoarderBugAI)
             {
                 var bug = (HoarderBugAI)enemy;
-                MakeBugsDance(bug.transform, 10, 10);
-                BecomeFriends(bug, 15f, true, playerHeldBy);
+                MakeBugsDance(bug.transform, 10, 100);
+                BecomeFriends(bug, 120f, true, this, playerHeldBy);
             }
         }
 
-        public static void BecomeFriends(HoarderBugAI mainBug, float friendAmount, bool allBugs = true, PlayerControllerB player = null)
+        public static void BecomeFriends(HoarderBugAI mainBug, float friendAmount, bool allBugs = true, GrabbableObject held = null, PlayerControllerB player = null)
         {
-            mainBug.BecomeFriends(friendAmount, player);
+            mainBug.BecomeFriends(friendAmount, held, player);
 
             if (allBugs)
                 foreach (HoarderBugAI bug in FindObjectsOfType<HoarderBugAI>())
                 {
-                    bug.BecomeFriends(friendAmount, player);
+                    bug.BecomeFriends(friendAmount, held, player);
                 }
         }
         public static void MakeBugsDance(Transform pos, int time, float range)
@@ -73,7 +72,7 @@ namespace LC_HoardingBugSnacks.Patches
 
     public static class ExtensionMethod
     {
-        public static void BecomeFriends(this HoarderBugAI original, float amount, PlayerControllerB player = null)
+        public static void BecomeFriends(this HoarderBugAI original, float amount, GrabbableObject held = null,  PlayerControllerB player = null)
         {
             Type type = typeof(HoarderBugAI);
 
@@ -89,16 +88,16 @@ namespace LC_HoardingBugSnacks.Patches
                 field.SetValue(original, currentValue - amount);
             }
 
+            original.targetItem = held;
             original.angryAtPlayer = null;
             if(player != null) original.watchingPlayer = player;
-            original.angryTimer -= amount;
 
             BugSnacks.BugHappiness += amount;
         }
 
         public static void StartDance(this HoarderBugAI original, float duration)
         {
-            float startingY = 1.5863f;
+            float startingY = 2;
             original.StopAllCoroutines();
             original.BecomeFriends(.5f);
 
